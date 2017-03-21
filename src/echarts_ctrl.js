9ea31edf98c619c23cfe82e6e6583dd6268ca18e
Wang <qianxiang.wang@hpe.com>
2017-03-21 18:33:27 +0800

    update editer.html

--- src/echarts_ctrl.js
+++ src/echarts_ctrl.js
@@ -7,87 +7,19 @@ export class EchartsCtrl extends MetricsPanelCtrl {
     constructor($scope, $injector) {
         super($scope, $injector);
 
-        const dataStyle = {
-            normal: {
-                label: { show: false },
-                labelLine: { show: false },
-                shadowBlur: 40,
-                shadowColor: 'rgba(40, 40, 40, 0.5)',
-            }
-        };
-        const placeHolderStyle = {
-            normal: {
-                label: { show: false },
-                labelLine: { show: false }
-            },
-            emphasis: {
-                color: 'rgba(0,0,0,0)'
-            }
-        };
         const panelDefaults = {
-            options: {
+            EchartsOption: {
                 color: ['#85b6b2', '#6d4f8d','#cd5e7e', '#e38980','#f7db88'],
-                tooltip: {
-                    show: true,
-                    formatter: "{a} <br/>{b} : {c}TB ({d}%)"
-                },
                 legend: {
                     textStyle: {
                         color: 'rgba(255, 255, 255, 1)'
                     }
                 },
-                series: [{
-                    name: '全部存储',
-                    type: 'pie',
-                    clockWise: false,
-                    hoverAnimation: false,
-                    radius: ["50%", "60%"],
-                    itemStyle: dataStyle,
-                    data: [{
-                        value: 975,
-                        name: '已用总量'
-                    }, {
-                        value: 535,
-                        name: '剩余',
-                        itemStyle: placeHolderStyle
-                    }]
-                }, {
-                    name: '端口占用',
-                    type: 'pie',
-                    clockWise: false,
-                    hoverAnimation: false,
-                    radius: ["40%", "50%"],
-                    itemStyle: dataStyle,
-                    data: [{
-                        value: 1673,
-                        name: '已用端口'
-                    }, {
-                        value: 797,
-                        name: '剩余',
-                        itemStyle: placeHolderStyle
-                    }]
-                }, {
-                    name: '主机存储',
-                    type: 'pie',
-                    clockWise: false,
-                    hoverAnimation: false,
-                    radius: ["30%", "40%"],
-                    itemStyle: dataStyle,
-                    data: [{
-                        value: 367,
-                        name: '已用存储'
-                    }, {
-                        value: 266,
-                        name: '剩余',
-                        itemStyle: placeHolderStyle
-                    }]
-                }]
+                series: []
             }
         };
 
         _.defaults(this.panel, panelDefaults);
-
-        console.log(this.panel.options);
 
         this.events.on('init-edit-mode', this.onInitEditMode.bind(this));
         this.events.on('panel-initialized', this.render.bind(this));
@@ -99,13 +31,13 @@ export class EchartsCtrl extends MetricsPanelCtrl {
     }
 
     onInitEditMode() {
-        this.addEditorTab('Options', 'public/plugins/grafana-demo-panel/editor.html', 2);
+        this.addEditorTab('EchartsOption', 'public/plugins/grafana-echarts-panel/editor.html', 2);
     }
 
     link(scope, elem, attrs, ctrl) {
         const $panelContainer = elem.find('.echarts_container')[0];
 
-        console.log(elem.clientWidth);
+        // console.log(elem.clientWidth);
 
         $panelContainer.style.width = $panelContainer.parentNode.parentNode.clientWidth + 'px';
         $panelContainer.style.height = $panelContainer.parentNode.parentNode.clientHeight + 'px';
@@ -113,11 +45,11 @@ export class EchartsCtrl extends MetricsPanelCtrl {
         let myChart = echarts.init($panelContainer);
 
         function render() {
-            // if (!ctrl.panel.option) {
+            // if (!ctrl.panel.EchartsOption) {
             //     return; }
+            console.log(ctrl.panel.EchartsOption);
             myChart.resize();
-            myChart.setOption(ctrl.panel.options);
-            console.log(ctrl.panel.options);
+            myChart.setOption(eval("(" + ctrl.panel.EchartsOption + ")"));
         }
 
         this.events.on('render', function () {

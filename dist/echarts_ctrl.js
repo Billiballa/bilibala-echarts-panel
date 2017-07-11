b4dd6637ee3b42873de1aca5b4edc85f84698fce
Wang <qianxiang.wang@hpe.com>
2017-07-11 16:12:32 +0800

    修改地图引入，改变获取数据方式

--- dist/echarts_ctrl.js
+++ dist/echarts_ctrl.js
@@ -1,9 +1,9 @@
 'use strict';
 
-System.register(['app/plugins/sdk', 'lodash', './libs/echarts.min', './libs/echarts-liquidfill.min', './libs/echarts-wordcloud.min', './libs/dark', './libs/china', './libs/beijing', './style.css!'], function (_export, _context) {
+System.register(['app/plugins/sdk', 'lodash', './libs/echarts.min', './libs/echarts-liquidfill.min', './libs/echarts-wordcloud.min', './libs/dark', './style.css!'], function (_export, _context) {
     "use strict";
 
-    var PanelCtrl, _, echarts, _createClass, EchartsCtrl;
+    var MetricsPanelCtrl, _, echarts, _createClass, EchartsCtrl;
 
     function _classCallCheck(instance, Constructor) {
         if (!(instance instanceof Constructor)) {
@@ -37,12 +37,12 @@ System.register(['app/plugins/sdk', 'lodash', './libs/echarts.min', './libs/echa
 
     return {
         setters: [function (_appPluginsSdk) {
-            PanelCtrl = _appPluginsSdk.PanelCtrl;
+            MetricsPanelCtrl = _appPluginsSdk.MetricsPanelCtrl;
         }, function (_lodash) {
             _ = _lodash.default;
         }, function (_libsEchartsMin) {
             echarts = _libsEchartsMin.default;
-        }, function (_libsEchartsLiquidfillMin) {}, function (_libsEchartsWordcloudMin) {}, function (_libsDark) {}, function (_libsChina) {}, function (_libsBeijing) {}, function (_styleCss) {}],
+        }, function (_libsEchartsLiquidfillMin) {}, function (_libsEchartsWordcloudMin) {}, function (_libsDark) {}, function (_styleCss) {}],
         execute: function () {
             _createClass = function () {
                 function defineProperties(target, props) {
@@ -62,8 +62,8 @@ System.register(['app/plugins/sdk', 'lodash', './libs/echarts.min', './libs/echa
                 };
             }();
 
-            _export('EchartsCtrl', EchartsCtrl = function (_PanelCtrl) {
-                _inherits(EchartsCtrl, _PanelCtrl);
+            _export('EchartsCtrl', EchartsCtrl = function (_MetricsPanelCtrl) {
+                _inherits(EchartsCtrl, _MetricsPanelCtrl);
 
                 function EchartsCtrl($scope, $injector) {
                     _classCallCheck(this, EchartsCtrl);
@@ -71,21 +71,26 @@ System.register(['app/plugins/sdk', 'lodash', './libs/echarts.min', './libs/echa
                     var _this = _possibleConstructorReturn(this, (EchartsCtrl.__proto__ || Object.getPrototypeOf(EchartsCtrl)).call(this, $scope, $injector));
 
                     var panelDefaults = {
-                        EchartsOption: 'console.log(JSON.stringify(echartsData)); \n option = {};',
-                        valueMaps: [],
-                        sensors: [],
+                        EchartsOption: 'console.log(JSON.stringify(echartsData));\n\n option = {};',
+                        IS_MAP: false,
+                        map: '',
+                        USE_URL: false,
                         url: '',
                         request: '',
                         updateInterval: 10000
                     };
 
-                    _.defaults(_this.panel, panelDefaults);
-                    _.defaults(_this.panel.EchartsOption, panelDefaults.EchartsOption);
+                    _this.maps = ['世界', '中国', '北京'];
 
+                    _.defaults(_this.panel, panelDefaults);
+
+                    _this.events.on('data-received', _this.onDataReceived.bind(_this));
+                    _this.events.on('data-error', _this.onDataError.bind(_this));
+                    _this.events.on('data-snapshot-load', _this.onDataReceived.bind(_this));
                     _this.events.on('init-edit-mode', _this.onInitEditMode.bind(_this));
                     _this.events.on('panel-initialized', _this.render.bind(_this));
 
-                    _this.updateClock();
+                    _this.updateData();
                     return _this;
                 }
 
@@ -93,8 +98,8 @@ System.register(['app/plugins/sdk', 'lodash', './libs/echarts.min', './libs/echa
 
 
                 _createClass(EchartsCtrl, [{
-                    key: 'updateClock',
-                    value: function updateClock() {
+                    key: 'updateData',
+                    value: function updateData() {
                         var _this2 = this;
 
                         var that = this,
@@ -109,8 +114,8 @@ System.register(['app/plugins/sdk', 'lodash', './libs/echarts.min', './libs/echa
                         xmlhttp.onreadystatechange = function () {
                             if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
                                 if (!JSON.parse(xmlhttp.responseText).success) return;
-                                that.data = JSON.parse(xmlhttp.responseText).data;
-                                that.dataChanged();
+                                that.UrlData = JSON.parse(xmlhttp.responseText).data;
+                                that.onDataReceived();
                             }
                         };
 
@@ -120,20 +125,59 @@ System.register(['app/plugins/sdk', 'lodash', './libs/echarts.min', './libs/echa
                         }
 
                         this.$timeout(function () {
-                            _this2.updateClock();
+                            _this2.updateData();
                         }, that.panel.updateInterval);
                     }
                 }, {
-                    key: 'dataChanged',
-                    value: function dataChanged() {
+                    key: 'onDataReceived',
+                    value: function onDataReceived(dataList) {
+                        this.data = this.panel.USE_URL ? this.UrlData : dataList;
                         this.IS_DATA_CHANGED = true;
                         this.render();
                         this.IS_DATA_CHANGED = false;
                     }
                 }, {
+                    key: 'onDataError',
+                    value: function onDataError(err) {
+                        this.render();
+                    }
+                }, {
                     key: 'onInitEditMode',
                     value: function onInitEditMode() {
-                        this.addEditorTab('Options', 'public/plugins/grafana-echarts-panel/editor.html', 2);
+                        this.addEditorTab('Ecahrts配置', 'public/plugins/grafana-echarts-panel/editor.html', 2);
+                    }
+                }, {
+                    key: 'importMap',
+                    value: function importMap() {
+                        if (!this.IS_MAP) return;
+                        switch (this.panel.map) {
+                            case '世界':
+                                System.import(this.getPanelPath() + 'libs/world.js');
+                                break;
+                            case '中国':
+                                System.import(this.getPanelPath() + 'libs/china.js');
+                                break;
+                            case '北京':
+                                System.import(this.getPanelPath() + 'libs/beijing.js');
+                                break;
+                            // case '百度地图':
+                            // System.import('http://gallery.echartsjs.com/dep/echarts/latest/extension/bmap.min.js');
+                            // System.import('http://api.map.baidu.com/getscript?v=2.0&ak=ZUONbpqGBsYGXNIYHicvbAbM&services=&t=20170703123905');
+                            // document.body.innerHTML+='<script src="http://gallery.echartsjs.com/dep/echarts/latest/extension/bmap.min.js"><script/>';
+                            // (function(){
+                            //     window.BMap_loadScriptTime = (new Date()).getTime();
+                            //     document.body.innerHTML += '<script type="text/javascript" src="http://api.map.baidu.com/getscript?v=2.0&ak=ZUONbpqGBsYGXNIYHicvbAbM&services=&t=20170705114645"></script>';
+                            // })();
+                            // break;
+                            default:
+                                break;
+                        }
+                    }
+                }, {
+                    key: 'getPanelPath',
+                    value: function getPanelPath() {
+                        // the system loader preprends publib to the url, add a .. to go back one level
+                        return '../' + grafanaBootData.settings.panels[this.pluginId].baseUrl + '/';
                     }
                 }, {
                     key: 'link',
@@ -144,38 +188,43 @@ System.register(['app/plugins/sdk', 'lodash', './libs/echarts.min', './libs/echa
 
                         ctrl.IS_DATA_CHANGED = true;
 
-                        //init height
-                        var height = ctrl.height || panel.height || ctrl.row.height;
-                        if (_.isString(height)) {
-                            height = parseInt(height.replace('px', ''), 10);
+                        function setHeight() {
+                            var height = ctrl.height || panel.height || ctrl.row.height;
+                            if (_.isString(height)) {
+                                height = parseInt(height.replace('px', ''), 10);
+                            }
+                            // height -= 7;
+                            // height -= ctrl.panel.title ? 25 : 9;
+                            $panelContainer.style.height = height + 'px';
                         }
-                        height -= 5;
-                        height -= ctrl.panel.title ? 24 : 9;
-                        $panelContainer.style.height = height + 'px';
 
-                        //init width
-                        var width = document.body.clientWidth;
-                        width = (width - 5.6 * 2) * ctrl.panel.span / 12 - 5.6 * 2 - 1 * 2 - 10 * 2;
-                        $panelContainer.style.width = width + 'px';
-
-                        //init echarts
-                        var myChart = echarts.init($panelContainer, 'dark');
-
-                        //替代eval
-                        // function evil(fn) {
-                        //     var Fn = Function; //一个变量指向Function，防止有些前端编译工具报错
-                        //     return new Fn('return ' + fn)();
+                        // function setWidth() {
+                        //     let width = document.body.clientWidth;
+                        //     width = (width - 5.6 * 2) * ctrl.panel.span / 12 - 5.6 * 2 - 1 * 2 - 10 * 2;
+                        //     $panelContainer.style.width = width + 'px';
                         // }
 
-                        // 计时器容器，防止重复触发计时事件
-                        var callInterval = function () {
+                        setHeight();
+                        // setWidth();
+
+                        var myChart = echarts.init($panelContainer, 'dark');
+
+                        ctrl.importMap();
+
+                        // bad hank
+                        setTimeout(function () {
+                            myChart.resize();
+                        }, 1000);
+
+                        // 防止重复触发事件
+                        var callInterval = function callInterval() {
                             var timeout, result;
 
                             function func(callBack, interval) {
                                 var context = this; // jshint ignore:line
                                 var args = arguments;
 
-                                if (timeout) clearTimeout(timeout);
+                                if (timeout) clearInterval(timeout);
 
                                 timeout = setInterval(function () {
                                     result = callBack.apply(context, args);
@@ -188,9 +237,12 @@ System.register(['app/plugins/sdk', 'lodash', './libs/echarts.min', './libs/echa
                         }();
 
                         function render() {
+
                             if (!myChart) {
                                 return;
                             }
+
+                            setHeight();
                             myChart.resize();
 
                             if (ctrl.IS_DATA_CHANGED) {
@@ -198,7 +250,6 @@ System.register(['app/plugins/sdk', 'lodash', './libs/echarts.min', './libs/echa
                                 echartsData = ctrl.data;
 
                                 eval(ctrl.panel.EchartsOption); // jshint ignore:line
-                                // evil(ctrl.panel.EchartsOption);
 
                                 myChart.setOption(option);
                             }
@@ -212,7 +263,7 @@ System.register(['app/plugins/sdk', 'lodash', './libs/echarts.min', './libs/echa
                 }]);
 
                 return EchartsCtrl;
-            }(PanelCtrl));
+            }(MetricsPanelCtrl));
 
             _export('EchartsCtrl', EchartsCtrl);
 
--- src/echarts_ctrl.js
+++ src/echarts_ctrl.js
@@ -1,38 +1,41 @@
-import { PanelCtrl } from 'app/plugins/sdk';
+import { MetricsPanelCtrl } from 'app/plugins/sdk';
 import _ from 'lodash';
 import echarts from './libs/echarts.min';
 import './libs/echarts-liquidfill.min';
 import './libs/echarts-wordcloud.min';
 import './libs/dark';
-import './libs/china';
-import './libs/beijing';
 import './style.css!';
 
-export class EchartsCtrl extends PanelCtrl {
+export class EchartsCtrl extends MetricsPanelCtrl {
 
     constructor($scope, $injector) {
         super($scope, $injector);
 
         const panelDefaults = {
-            EchartsOption: 'console.log(JSON.stringify(echartsData)); \n option = {};',
-            valueMaps: [],
-            sensors: [],
+            EchartsOption: 'console.log(JSON.stringify(echartsData));\n\n option = {};',
+            IS_MAP: false,
+            map: '',
+            USE_URL: false,
             url: '',
             request: '',
             updateInterval: 10000
         };
 
-        _.defaults(this.panel, panelDefaults);
-        _.defaults(this.panel.EchartsOption, panelDefaults.EchartsOption);
+        this.maps = ['世界', '中国', '北京'];
 
+        _.defaults(this.panel, panelDefaults);
+
+        this.events.on('data-received', this.onDataReceived.bind(this));
+        this.events.on('data-error', this.onDataError.bind(this));
+        this.events.on('data-snapshot-load', this.onDataReceived.bind(this));
         this.events.on('init-edit-mode', this.onInitEditMode.bind(this));
         this.events.on('panel-initialized', this.render.bind(this));
 
-        this.updateClock();
+        this.updateData();
     }
 
     //post请求
-    updateClock() {
+    updateData() {
         let that = this, xmlhttp;
 
         if (window.XMLHttpRequest) {
@@ -44,8 +47,8 @@ export class EchartsCtrl extends PanelCtrl {
         xmlhttp.onreadystatechange = function () {
             if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
                 if (!JSON.parse(xmlhttp.responseText).success) return;
-                that.data = JSON.parse(xmlhttp.responseText).data;
-                that.dataChanged();
+                that.UrlData = JSON.parse(xmlhttp.responseText).data;
+                that.onDataReceived();
             }
         };
 
@@ -54,17 +57,53 @@ export class EchartsCtrl extends PanelCtrl {
             xmlhttp.send(that.panel.request);
         }
 
-        this.$timeout(() => { this.updateClock(); }, that.panel.updateInterval);
+        this.$timeout(() => { this.updateData(); }, that.panel.updateInterval);
     }
 
-    dataChanged() {
+    onDataReceived(dataList) {
+        this.data = this.panel.USE_URL ? this.UrlData : dataList;
         this.IS_DATA_CHANGED = true;
         this.render();
         this.IS_DATA_CHANGED = false;
     }
 
+    onDataError(err) {
+        this.render();
+    }
+
     onInitEditMode() {
-        this.addEditorTab('Options', 'public/plugins/grafana-echarts-panel/editor.html', 2);
+        this.addEditorTab('Ecahrts配置', 'public/plugins/grafana-echarts-panel/editor.html', 2);
+    }
+
+    importMap() {
+        if (!this.IS_MAP) return;
+        switch (this.panel.map) {
+            case '世界':
+                System.import(this.getPanelPath() + 'libs/world.js');
+                break;
+            case '中国':
+                System.import(this.getPanelPath() + 'libs/china.js');
+                break;
+            case '北京':
+                System.import(this.getPanelPath() + 'libs/beijing.js');
+                break;
+            // case '百度地图':
+            // System.import('http://gallery.echartsjs.com/dep/echarts/latest/extension/bmap.min.js');
+            // System.import('http://api.map.baidu.com/getscript?v=2.0&ak=ZUONbpqGBsYGXNIYHicvbAbM&services=&t=20170703123905');
+            // document.body.innerHTML+='<script src="http://gallery.echartsjs.com/dep/echarts/latest/extension/bmap.min.js"><script/>';
+            // (function(){
+            //     window.BMap_loadScriptTime = (new Date()).getTime();
+            //     document.body.innerHTML += '<script type="text/javascript" src="http://api.map.baidu.com/getscript?v=2.0&ak=ZUONbpqGBsYGXNIYHicvbAbM&services=&t=20170705114645"></script>';
+            // })();
+            // break;
+            default:
+                break;
+        }
+    }
+
+    getPanelPath() {
+        // the system loader preprends publib to the url, add a .. to go back one level
+        return '../' + grafanaBootData.settings.panels[this.pluginId].baseUrl + '/';
     }
 
     link(scope, elem, attrs, ctrl) {
@@ -74,38 +113,43 @@ export class EchartsCtrl extends PanelCtrl {
 
         ctrl.IS_DATA_CHANGED = true;
 
-        //init height
-        let height = ctrl.height || panel.height || ctrl.row.height;
-        if (_.isString(height)) {
-            height = parseInt(height.replace('px', ''), 10);
+        function setHeight() {
+            let height = ctrl.height || panel.height || ctrl.row.height;
+            if (_.isString(height)) {
+                height = parseInt(height.replace('px', ''), 10);
+            }
+            // height -= 7;
+            // height -= ctrl.panel.title ? 25 : 9;
+            $panelContainer.style.height = height + 'px';
         }
-        height -= 5;
-        height -= ctrl.panel.title ? 24 : 9;
-        $panelContainer.style.height = height + 'px';
 
-        //init width
-        let width = document.body.clientWidth;
-        width = (width - 5.6 * 2) * ctrl.panel.span / 12 - 5.6 * 2 - 1 * 2 - 10 * 2;
-        $panelContainer.style.width = width + 'px';
-
-        //init echarts
-        let myChart = echarts.init($panelContainer, 'dark');
-
-        //替代eval
-        // function evil(fn) {
-        //     var Fn = Function; //一个变量指向Function，防止有些前端编译工具报错
-        //     return new Fn('return ' + fn)();
+        // function setWidth() {
+        //     let width = document.body.clientWidth;
+        //     width = (width - 5.6 * 2) * ctrl.panel.span / 12 - 5.6 * 2 - 1 * 2 - 10 * 2;
+        //     $panelContainer.style.width = width + 'px';
         // }
 
-        // 计时器容器，防止重复触发计时事件
-        var callInterval = function () {
+        setHeight();
+        // setWidth();
+
+        let myChart = echarts.init($panelContainer, 'dark');
+
+        ctrl.importMap();
+
+        // bad hank
+        setTimeout(function () {
+            myChart.resize();
+        }, 1000);
+
+        // 防止重复触发事件
+        var callInterval = function callInterval() {
             var timeout, result;
 
             function func(callBack, interval) {
                 var context = this; // jshint ignore:line
                 var args = arguments;
 
-                if (timeout) clearTimeout(timeout);
+                if (timeout) clearInterval(timeout);
 
                 timeout = setInterval(function () {
                     result = callBack.apply(context, args);
@@ -118,9 +162,12 @@ export class EchartsCtrl extends PanelCtrl {
         }();
 
         function render() {
+
             if (!myChart) {
                 return;
             }
+
+            setHeight();
             myChart.resize();
 
             if (ctrl.IS_DATA_CHANGED) {
@@ -128,7 +175,6 @@ export class EchartsCtrl extends PanelCtrl {
                 echartsData = ctrl.data;
 
                 eval(ctrl.panel.EchartsOption); // jshint ignore:line
-                // evil(ctrl.panel.EchartsOption);
 
                 myChart.setOption(option);
             }

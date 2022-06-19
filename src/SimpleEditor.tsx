5f1d4f8d945d9fc5e4a5e0d20eb5f3bf19b5f197
Mikhail <mikhail@volkovlabs.io>
2022-06-18 21:01:46 -0400

    Upgrade to Grafana 9

--- src/SimpleEditor.tsx
+++ src/SimpleEditor.tsx
@@ -1,9 +1,9 @@
-import React, { PureComponent } from 'react';
-import { Field, Switch } from '@grafana/ui';
-import { PanelEditorProps } from '@grafana/data';
-import { SimpleOptions } from './types';
-import MyField from './components/MyField';
 import { FieldCMEditor } from 'components/FieldCMEditor';
+import React, { PureComponent } from 'react';
+import { PanelEditorProps } from '@grafana/data';
+import { Field, Switch } from '@grafana/ui';
+import MyField from './components/MyField';
+import { SimpleOptions } from './types';
 
 export class SimpleEditor extends PureComponent<PanelEditorProps<SimpleOptions>> {
   onChange(key: string, value: any) {
@@ -19,17 +19,16 @@ export class SimpleEditor extends PureComponent<PanelEditorProps<SimpleOptions>>
           description="Use default theme or follow theme of grafana (light or dark)."
         >
           <Switch
-            css=""
             checked={this.props.options.followTheme}
             value={this.props.options.followTheme}
-            onChange={e => this.onChange('followTheme', (e.target as HTMLInputElement).checked)}
+            onChange={(e) => this.onChange('followTheme', (e.target as HTMLInputElement).checked)}
           />
         </FieldEl>
         <FieldEl
           label="Echarts Option"
           description="Return options called by echarts or just use echartsInstance.setOption(...)."
         >
-          <FieldCMEditor value={this.props.options.getOption} onChange={v => this.onChange('getOption', v)} />
+          <FieldCMEditor value={this.props.options.getOption} onChange={(v) => this.onChange('getOption', v)} />
         </FieldEl>
       </>
     );
